6a720413d8a9f393e3dd84dc9e8f501780603d9e
thanhtranndang <thanhtranndang@gmail.com>
2024-05-02 23:28:59 +0700

    first lesson

---
+++ class & object/Exercise02.01.cpp
@@ -0,0 +1,17 @@
+/*
+
+Thiết kế class thể hiện một đối tượng Học sinh trong thực tế
+# Các thuộc tính và data type tương ứng
+    mã học sinh - int
+    tên - string
+    tuổi - int
+    Quê quán
+    chiều cao - float
+    cân nặng - float
+điểm trung bình 8môn học Toán, Lý, Hoá, Văn, Anh, Sinh, Sử, Địa - mảng/vector
+# Phương thức
+    Giới thiệu bản thân gồm tên, tuổi, quê quán
+    Show bảng điểm của 8 môn học
+    Show học lực, nếu trung bình 8 môn trên 8 là giỏi, từ 5 đến 8 là khá và còn lại là trung bình
+    Tự tạo hàm main để test các phương thức. Lưu ý: các đối tượng cần cấp phát động
+*/
---
+++ class & object/property.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+class BodySize {
+    public:
+        int bodySize[3];
+        float weight;
+};
+
+
+
+class Woman{
+    public:
+        std::string fullName;
+        BodySize bodySize;
+
+        void makeUp() {
+
+        }
+
+        void Introduce() {
+            
+        }
+
+
+};
+
+int main() {
+
+
+    return 0;
+}
