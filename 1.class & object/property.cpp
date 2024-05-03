1668ed9fffcabd05289acea14bc7b0f4b899a42f
thanhtranndang <thanhtranndang@gmail.com>
2024-05-03 22:02:31 +0700

    class & obj

---
+++ 1.class & object/property.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+
+class BodySize
+{
+public:
+    int bodySize[3];
+    float weight;
+};
+class Woman
+{
+public:
+    std::string fullName;
+    BodySize bodySize;
+
+    void makeUp()
+    {
+    }
+
+    void Introduce()
+    {
+        std::cout << "Hi! My name is X";
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
+
+/*
+    Bản chất class là version "nâng cấp" của struct
+    - Class có thể chứa các phương thức (method) và thuộc tính (property)
+    - Struct chỉ chứa thuộc tính (property)
+    - Struct và Class đều có thể chứa các biến, hàm, phương thức, thuộc tính
+    - Struct và Class đều có thể kế thừa từ 1 class khác
+    - Struct và Class đều có thể implement interface
+*/
